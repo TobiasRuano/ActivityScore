04bbaab81105394d6a544c5e8e6838b9b420ac2b
Tobias Ruano <ruano.t10@gmail.com>
2022-03-05 18:01:05 -0300

    AS-54 Fix score calculation

--- ActivityScore/Model/Fitness.swift
+++ ActivityScore/Model/Fitness.swift
@@ -28,37 +28,6 @@ class Fitness {
     var dailyData: [Date: DailyData] = [:]
     private var userGoals: Goals!
 
-//    func setItemInDailyData(amounts: [Date: Int], type: DailyDataType) {
-//        for element in amounts {
-//            let key = element.key
-//            if dailyData[key] != nil {
-//                switch type {
-//                case .steps:
-//                    dailyData[key]?.steps = element.value
-//                case .distance:
-//                    dailyData[key]?.distance = element.value
-//                case .calories:
-//                    dailyData[key]?.calories = element.value
-//                case .exercise:
-//                    dailyData[key]?.exercise = element.value
-//                }
-//            } else {
-//                var newDayData: DailyData
-//                switch type {
-//                case .steps:
-//                    newDayData = DailyData(score: 0, steps: element.value, distance: 0, calories: 0, exercise: 0)
-//                case .distance:
-//                    newDayData = DailyData(score: 0, steps: 0, distance: element.value, calories: 0, exercise: 0)
-//                case .calories:
-//                    newDayData = DailyData(score: 0, steps: 0, distance: 0, calories: element.value, exercise: 0)
-//                case .exercise:
-//                    newDayData = DailyData(score: 0, steps: 0, distance: 0, calories: 0, exercise: element.value)
-//                }
-//                dailyData[key] = newDayData
-//            }
-//        }
-//    }
-
     func getFitnessData() -> [Date: DailyData] {
         return dailyData
     }
@@ -75,7 +44,7 @@ class Fitness {
                 dailyData[element.key]?.score = (Int(70.0 + (0.10 * Double(valueCopy))))
             }
 
-            if element.value.score > 100 {
+			if dailyData[element.key]!.score > 100 {
                 dailyData[element.key]?.score = 100
             }
         }
